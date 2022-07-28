11dbb90cbe3bff5584bace311969abb894c7adef
Muhammad Aahad <maahad@asaadshub.com>
2022-07-29 04:03:39 +0500

    font family and theme provider added

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,27 @@ import logo from './logo.svg';
 import './App.css';
 import renderRoutes from './renderRoutes';
 import routes from './routes';
-
+import { createTheme } from '@mui/material';
+const theme = createTheme({
+  typography: {
+    fontFamily: "Poppins",
+  },
+  palette: {
+    primary:{
+      main:"#206ECFF",
+      secondary:"#fff",
+      sub:"#1F2024",
+    },
+    
+  },
+  
+});
 const App = () => {
 
   return (
-    <>
+    <ThemeProvider theme={theme}>
     {renderRoutes(routes)}
-    </>
+    </ThemeProvider>
   );
 };
 
